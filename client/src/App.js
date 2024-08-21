3951f81497677fb0715dd40ee7da5f6726d147b3
Karanpathania2205 <singhpathaniakaran@gmail.com>
2024-08-21 17:06:18 +0530

    Project Setup

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+   const [data, setData] = useState(null);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/api/data');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      }finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return <div>No data available</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Deployment Testing for a React-Flask Application of {data.name} , {data.state}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+@app.route('/api/data')
+@cross_origin(origin='*')
+def get_data():
+    # Define your data
+    data = {
+        'name': 'Sensing Local',
+        'state': 'Bangalore'
+    }
+    # Return JSON response
+    return jsonify(data)
+
+@app.route('/')
+@cross_origin(origin='*')
+def home():
+    return 'Basic React-Flask app for deployment testing'
+
+if __name__ == '__main__':
+    app.run(debug=True)
