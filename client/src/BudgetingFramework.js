f5f471ce893b220e2120ad5c0eda287fd03efe9f
Ishika Agarwal <agarwalishika20@gmail.com>
2024-10-14 15:18:08 +0530

    adding budgeting ui

--- client/src/BudgetingFramework.js
+++ client/src/BudgetingFramework.js
@@ -2,11 +2,18 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import Select from "react-select";
+
+import "./App.css";
 
 function BudgetingFramework() {
   const { templateId } = useParams(); // Retrieve the template ID from the URL
   const [solutions, setSolutions] = useState([]);
-  const [accordions, setAccordions] = useState([{ id: 1, selectedSolution: '' }]);
+  const [qualifiers, setQualifiers] = useState([]);
+  const [cards, setCards] = useState([{ id: 1, selectedSolution: '' }]); 
+  const [isCardOpen, setIsCardOpen] = useState({});
+  const [selectedLanduse, setSelectedLanduse] = useState([]);
+  const [selectedNorms, setSelectedNorms] = useState([]);
 
   useEffect(() => {
     axios.get(`http://localhost:8000/api/budgetingsolutions`)
@@ -18,38 +25,140 @@ function BudgetingFramework() {
       });
   });
 
-  const handleAccordionAdd = () => {
-    setAccordions([...accordions, { id: accordions.length + 1, selectedSolution: '' }]);
-  };
+  const fetchQualifiers = (templateId) => {
+    axios.get(`http://localhost:8000/api/qualifiers/${templateId}`)
+      .then(response => {
+        setQualifiers(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching solutions:', error);
+      });
+  }
 
-  const handleAccordionDelete = (id) => {
-    setAccordions(accordions.filter(accordion => accordion.id !== id));
+  const handleCardToggle = (id) => {
+    setIsCardOpen(prevState => ({
+      ...prevState,
+      [id]: !prevState[id],  // Toggle the state for this card
+    }));
   };
 
   const handleSolutionChange = (id, value) => {
-    setAccordions(accordions.map(accordion =>
-      accordion.id === id ? { ...accordion, selectedSolution: value } : accordion
-    ));
+    setCards(cards.map(card => card.id === id ? { ...card, selectedSolution: value } : card));
+    fetchQualifiers(templateId);
   };
+
+  const handleCardAdd = () => {
+    setCards([...cards, { id: cards.length + 1, selectedSolution: '' }]);
+  };
+
+  const handleCardDelete = (id) => {
+    setCards(cards.filter(card => card.id !== id));  // Remove the card with the matching id
+  };
+
+  const handleLanduseChange = (selectedOptions) => {
+    setSelectedLanduse(selectedOptions.map((option) => option.value))
+  }
+
+  const handleNormChange = (selectedOptions) => {
+    setSelectedNorms(selectedOptions.map((option) => option.value));
+  }
 
   return (
     <div>
       <h1>Budgeting Framework</h1>
-      {accordions.map((accordion, index) => (
-        <div key={accordion.id} className="solution-accordion">
-          <select
-            value={accordion.selectedSolution}
-            onChange={(e) => handleSolutionChange(accordion.id, e.target.value)}
-          >
-            <option value="">Select a solution</option>
-            {solutions.map((solution, idx) => (
-              <option key={idx} value={solution}>{solution}</option>
-            ))}
-          </select>
-          <button onClick={() => handleAccordionDelete(accordion.id)}>Delete</button>
+      {cards.map(card => (
+        <div key={card.id} className="card">
+          <div className="card-header" onClick={() => handleCardToggle(card.id)}>
+            <span>Select Solution</span>
+            <select
+              value={card.selectedSolution}
+              onChange={(e) => handleSolutionChange(card.id, e.target.value)}
+            >
+              <option value="">Select a solution</option>
+              {solutions.map((solution, idx) => (
+                <option key={idx} value={solution}>{solution}</option>
+              ))}
+            </select>
+            <button 
+              className="delete-button" 
+              onClick={(e) => {
+                e.stopPropagation(); // Prevent toggling when clicking delete
+                handleCardDelete(card.id);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+          <div className={`card-body ${isCardOpen[card.id] ? 'expanded' : ''}`}>
+          {qualifiers.filter((qual) => qual.qualifier_name === "Landuse Applicability").map(qual => (
+            <div className="qualifier-row">
+              <label>{qual.qualifier_name}: </label>
+              <Select 
+                isMulti
+                options={qual.qualifier_options.map(qoption => ({
+                  value: qoption,
+                  label: qoption
+                }))}
+                onChange={(e) => handleLanduseChange(e)}
+              />
+            </div>
+          ))}
+          {selectedLanduse.map(landuse => (
+            <div className='landuse-qualifiers'>
+              <h3>{landuse}</h3>
+              {qualifiers.filter((qual) => qual.qualifier_name !== "Landuse Applicability").map(qual => (
+                <div className="qualifier-row">
+                  <label className='qualifier-name'>{qual.qualifier_name}: </label>
+                  {qual.ismultiple ? (
+                    <div>
+                      {qual.qualifier_name === "Application Norms" ? (
+                        <div className='application-norms'>
+                          <Select 
+                            isMulti
+                            options={qual.qualifier_options.map(qoption => ({
+                              value: qoption,
+                              label: qoption
+                            }))}
+                            onChange={handleNormChange}
+                          />
+                            {selectedNorms.map(norm => (
+                              <div className='norms'>
+                                <span className='norm-name'>{norm}</span>
+                                <input className='norm-input' type="text" placeholder={`Input for ${norm}`} />
+                              </div>
+                            ))}
+                        </div>
+                      ): (
+                    <Select 
+                      isMulti
+                      options={qual.qualifier_options.map(qoption => ({
+                        value: qoption,
+                        label: qoption
+                      }))}
+                    />
+                    )}
+                    </div>
+                  ) : (
+                  <div className='qualifier-options'>
+                  <Select
+                    options=
+                      {qual.qualifier_options.map(qoption => ({
+                        value: qoption,
+                        label: qoption
+                      }))}
+                    className='qualifier-row-select'
+                  />
+                  <input className="qualifier-value" type="text"></input>
+                  </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+          </div>
         </div>
       ))}
-      <button classname="accordion-button" onClick={handleAccordionAdd}>Add Solution Accordion</button>
+      <button className="add-solution-button" onClick={handleCardAdd}>Add Solution</button>
     </div>
   );
 }
--- client/src/Template.js
+++ client/src/Template.js
@@ -1,12 +1,23 @@
 // src/pages/Template.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import './App.css'
 
 function Template() {
   const [city, setCity] = useState('');
   const [templateId, setTemplateId] = useState('');
   const navigate = useNavigate();
+  const tableData = [
+    { siNo: 1, templateId: 'Template1', lastModified: '10/10/2024', signOffBy: 'System', mappedToCity: 'Bengaluru' },
+    { siNo: 2, templateId: 'Template2', lastModified: '07/10/2024', signOffBy: 'System', mappedToCity: 'Kochi' },
+  ];
 
   const handleViewEditClick = () => {
     if (templateId) {
@@ -18,9 +29,8 @@ function Template() {
   };
 
   return (
-    <div className="template-container">
-      <h1>Template</h1>
-      <label className='templatelabel'>
+    <div className='container'>
+      <label>
         Select City:
         <select className='templateselect' value={city} onChange={(e) => setCity(e.target.value)}>
           <option value="">Select City</option>
@@ -29,18 +39,51 @@ function Template() {
           {/* Add more cities as needed */}
         </select>
       </label>
-      <br />
-      <label className='templatelabel'>
-        Select Template:
-        <select className='templateselect' value={templateId} onChange={(e) => setTemplateId(e.target.value)}>
-          <option value="">Select Template</option>
-          <option value="1">Template 1</option>
-          <option value="2">Template 2</option>
-          {/* Add more templates as needed */}
-        </select>
-      </label>
-      <br />
-      <button className="view" onClick={handleViewEditClick}>View/Edit Template</button>
+      <div className='template-container-parent'>
+        <div className="template-container">
+        <button className="view" onClick={handleViewEditClick}>View / Set Up Template</button>
+          <br />
+          <label className='templatelabel'>
+            Select Template:
+            <select className='templateselect' value={templateId} onChange={(e) => setTemplateId(e.target.value)}>
+              <option value="">Select Template</option>
+              <option value="1">Template 1</option>
+              <option value="2">Template 2</option>
+              {/* Add more templates as needed */}
+            </select>
+          </label>
+          <br />
+        </div>
+        <div className="template-container">
+          <button className="view">Initiate Budgeting</button>
+        </div>
+      </div>
+      <div className='template-table'>
+      <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>SI No.</TableCell>
+            <TableCell>Template ID</TableCell>
+            <TableCell>Last Modified</TableCell>
+            <TableCell>Sign off by</TableCell>
+            <TableCell>Mapped to City</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tableData.map((row) => (
+            <TableRow key={row.siNo}>
+              <TableCell>{row.siNo}</TableCell>
+              <TableCell>{row.templateId}</TableCell>
+              <TableCell>{row.lastModified}</TableCell>
+              <TableCell>{row.signOffBy}</TableCell>
+              <TableCell>{row.mappedToCity}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+      </div>
     </div>
   );
 }
